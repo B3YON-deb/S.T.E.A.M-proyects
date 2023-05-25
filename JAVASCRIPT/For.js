e46f6c47e733121b05163cf545006c539148e25f
BEYON-DEB <supra00700@outlook.com>
2023-05-24 20:34:16 -0500

    ADD FOLDER JAVASCRIPT CONTENT For and Arrays

---
+++ JAVASCRIPT/For.js
@@ -0,0 +1,53 @@
+
+// _________________________________________________________________ -> :D
+
+// el bucle for consta de 3 parametros:
+
+// for (inicializacion;condicion;incremento){
+
+//}
+
+//seria tal cual...
+
+// for (i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+//el codigo visto anteriormente realizaria la impresion de los numeros del-
+//1-10
+
+// se puede saltar cualquier parte del bucle, siempre y cuando se haya-
+// definido antes la variable iteradora (i).
+
+// _________________________________________________________________
+
+// maneras de utilizar un ciclo for -> :D
+
+// _________________________________________________________________
+
+// primera forma -> :D (normal)
+
+// for(i=0;i<=10;i+=2){
+//     console.log(i)
+// }
+
+// _________________________________________________________________
+
+//segunda forma-> :D (obbiando el primer parametro, ya que esta definido en-
+// la variable (i))
+
+// let i = 0;
+// for(;i<=10;i++){
+//     console.log(i)
+// }
+
+// _________________________________________________________________
+
+//tercera forma -> :D (obbiando el primer y tercer parametro, ya que el primer-
+// parametro (inicializacion) lo toma del contenido de la variable i, y el tercer parametro-
+// (el incremento) se agrega dentro del console.log)
+
+// let i = 0
+// for(;i<=10;){
+//     console.log(i++)
+// }
